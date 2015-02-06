9da31708360f635e4754d29b307fde00e5dafb59
Forestmb <forestmb@gmail.com>
2015-02-05 23:18:03 -0500

    golint now fails the build.
    False positives should be added to the exclusion list in 'build.sh'.

--- build.sh
+++ build.sh
@@ -8,12 +8,33 @@ cd "${dir}"
 
 export PATH="${GOPATH}/bin:${PATH}"
 
+# List known golint warnings that are being suppressed
+#
+#   1. Consistent with OAuth 1.0 Consumer type
+lint_exclusions=(
+    "func GetRequestTokenAndUrl should be GetRequestTokenAndURL" 
+)
+
 echo "Running go get..."
 go get
 
 echo "Running golint..."
+ignore=
+for exclusion in "${lint_exclusions[@]}"; do
+    if [ -z "${ignore}" ]
+    then
+        ignore="${exclusion}"
+    else
+        ignore="${exclusion}|${ignore}"
+    fi
+done
 go get github.com/golang/lint/golint
-golint ./...
+if golint ./... | grep -v -E "${ignore}" | grep .
+then
+    echo "golint: warnings detected " 1>&2
+    echo "False positives should be added to the exclusion list in 'build.sh'" 1>&2
+    exit 2
+fi
 
 echo "Running go vet..."
 go vet ./...
